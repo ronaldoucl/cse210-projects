0b5ce6f882114fe26b480c5030157e62a462e5e5
ronaldoucl <ronaldocl9lda@gmail.com>
2024-06-15 01:00:07 -0600

    C# Prep 3—Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0; 
+        int numberOfGuesses = 0;
+        string option = "";
+        do {
+            Console.WriteLine("What is the magic number?");
+            int magicNumber = int.Parse(Console.ReadLine());
+            while (guess != magicNumber) {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+                if (guess < magicNumber) {
+                    Console.WriteLine("Higher");
+                } else if (guess > magicNumber) {
+                    Console.WriteLine("Lower");
+                }
+            }
+            Console.WriteLine("You guessed it!");
+            numberOfGuesses ++;
+            
+            Console.WriteLine("Do you want to play again? yes or no");
+            option = Console.ReadLine();
+        } while (option == "yes");
+
+        Console.WriteLine("Number of Guesses: " + numberOfGuesses);
+        Console.WriteLine("Thank you for playing!");
     }
 }
